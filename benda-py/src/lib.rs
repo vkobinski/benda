4a0f576ae84983639547d97a0cd65d3b34e38f00
vkobinski <victorkobinski@outlook.com>
2024-05-28 16:11:24 -0300

    Working AST parsing for decorator

--- benda-py/src/lib.rs
+++ benda-py/src/lib.rs
@@ -1,4 +1,7 @@
-use pyo3::{ prelude::*, types::PyFunction };
+use std::borrow::Borrow;
+
+use pyo3::{ conversion::FromPyObjectBound, prelude::*, types::{ PyFunction, PyTuple } };
+use python_ast::{parse, CodeGen, Name};
 use types::u24::u24;
 mod types;
 mod parser;
@@ -10,13 +13,32 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 
 #[pyfunction]
 fn bjit(fun: Bound<PyFunction>) -> PyResult<PyObject> {
-    let name = match fun.downcast::<PyFunction>() {
-        Ok(inner) => { 
-            println!("{:?}", inner.dir());
-            println!("{:?}", inner.getattr("file"));
+    let (name, filename) = match fun.downcast::<PyFunction>() {
+        Ok(inner) => {
+            let name = inner.getattr("__name__");
+            let filename = inner.getattr("__code__").unwrap().getattr("co_filename");
+
+            (name.unwrap(), filename.unwrap())
         }
         Err(_) => todo!(),
     };
+
+    let code = std::fs::read_to_string(filename.to_string()).unwrap();
+    let ast = parse(&code, "").unwrap();
+
+    for stmt in ast.raw.body {
+
+        match stmt.statement {
+            python_ast::StatementType::FunctionDef(fun_def) => {
+                if fun_def.name == name.to_string() {
+                    println!("{:?}", fun_def.body);
+
+                }
+            },
+            _ => {},
+        }
+        };
+
 
     todo!();
 }
